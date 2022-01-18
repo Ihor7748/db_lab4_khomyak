1e730b9dc0915e979b74295274d2aeca37a88765
Ihor7748 <43166074+Ihor7748@users.noreply.github.com>
2022-01-18 19:33:01 +0200

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -15,13 +15,18 @@ DECLARE
 BEGIN
 	IF EXISTS (SELECT region_id FROM region WHERE region_id = region_i) THEN
     	SELECT region_id, locality_name, country_name FROM region where region_id = region_i into id_, local_n, coun_n;
+		SELECT nextval('id_seq') INTO next_val;
 		INSERT INTO region (region_id, locality_name, country_name)
-			VALUES (nextval('id_seq'), local_n, coun_n);
+			VALUES (next_val, local_n, coun_n);
 		UPDATE region 
 		SET
 			locality_name = locality_n,
 			country_name = country_n
-		WHERE region_id = region_i;
+			WHERE region_id = region_i;
+		UPDATE market
+		SET
+			region_id = next_val
+			WHERE region_id = region_i;
     ELSE
 		INSERT INTO region (region_id, locality_name, coutry_name)
 			VALUES (region_i, locality_n, country_n);
