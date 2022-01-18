6e5e5d4580bf4c151ccc456c00c279e9bccd0c41
Ihor7748 <43166074+Ihor7748@users.noreply.github.com>
2022-01-18 16:57:13 +0200

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -22,25 +22,6 @@ CREATE TRIGGER autogen_region_id
 	EXECUTE PROCEDURE f_autogen_region_id();
 	
 	
-CREATE OR REPLACE FUNCTION f_autogen_produt_id() RETURNS TRIGGER AS
-$$
-DECLARE
-	next_val INT;
-BEGIN
-	SELECT nextval('id_seq') into next_val;
-	IF NEW.region_id >= next_val THEN
-		PERFORM setval('id_seq', new.region_id+1);
-	ELSIF NEW.region_id = 0 THEN
-		NEW.region_id = next_val;
-	END IF;
-	RETURN NEW;
-END;
-$$ LANGUAGE 'plpgsql';
 
-
-CREATE TRIGGER autogen_product_id
-	BEFORE INSERT ON product
-	FOR EACH ROW
-	EXECUTE PROCEDURE f_autogen_product_id();
 
 
