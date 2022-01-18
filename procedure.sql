2f79733aff853daa3f8dffeba97e975e19d68ab4
Ihor7748 <43166074+Ihor7748@users.noreply.github.com>
2022-01-18 17:19:39 +0200

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -1,19 +1,29 @@
 -- Add information about new market and location simultaneously while generatin location_id
-CREATE OR REPLACE PROCEDURE add_market_generating_location(
-    locality_n VARCHAR,
-    country_n VARCHAR,
-    market_i INT,
-    market_n VARCHAR,
-    market_t VARCHAR,
+CREATE OR REPLACE PROCEDURE force_region_id(
+	region_i INT,
+	locality_n VARCHAR(100),
+	counrty_n VARCHAR(100)
 )
 LANGUAGE 'plpgsql'
 AS $$
 DECLARE
-	max_id int := (SELECT MAX(location_id) FROM location);
+	id INT;
+	local_n VARCHAR(100);
+	coun_n VARCHAR(100);
+	next_val INT;
 BEGIN
-    INSERT INTO location (location_id, locality_name, country_name)
-        VALUES (max_id+1, locality_n, country_n);
-    INSERT INTO market (market_id, market_name, location_id, market_type)
-        VALUES (market_i, market_n, max_id+1, market_t) ;
+	IF EXISTS (SELECT region_id FROM region WHERE region_id = region_i) THEN
+    	SELECT region_id, locality_name, coutry_name FROM region into id, local_n, coun_n;
+		INSERT INTO region (region_id, locality_name, country_name)
+			VALUES (nextvalue('seq_id'), local_n, coun_n);
+		UPDATE region 
+		SET
+			locality_name = locality_n,
+			country_name = country_n
+		WHERE region_id = region_i;
+    ELSE
+		INSERT INTO region (region_id, locality_name, coutry_name)
+			VALUES (region_i, locality_n, country_n);
+	END IF;
 END;
 $$;
