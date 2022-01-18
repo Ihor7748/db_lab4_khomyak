a45f3dcaa0dfa2b75e933fc7531cccb78ceda941
Ihor7748 <43166074+Ihor7748@users.noreply.github.com>
2022-01-18 16:39:52 +0200

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,14 +1,46 @@
--- if no market insert market.location_id is not in location table add location
-create or replace function f_create_location_if_doesnt_exist() returns trigger as
-$$
-begin
-	if new.location_id not in (select location_id from location) then
-		insert into location (location_id, locality_name, country_name)
-			values (new.location_id, NULL, NULL);
-	end if;
-	return new;
-end;
-$$ language 'plpgsql';
+CREATE SEQUENCE id_seq START WITH 1 INCREMENT 1;
 
-create trigger create_location_if_doesnt_exist before insert on market 
-for each row execute procedure f_create_location_if_doesnt_exist();
+CREATE OR REPLACE FUNCTION f_autogen_region_id() RETURNS TRIGGER AS
+$$
+DECLARE
+	next_val INT;
+BEGIN
+	SELECT nextval('id_seq') into next_val;
+	IF NEW.region_id >= next_val THEN
+		PERFORM setval('id_seq', new.product_id+1);
+	ELSIF NEW.product_id = 0 THEN
+		NEW.product_id = next_val;
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+
+CREATE TRIGGER autogen_region_id
+	BEFORE INSERT ON region
+	FOR EACH ROW
+	EXECUTE PROCEDURE f_autogen_region_id();
+	
+	
+CREATE OR REPLACE FUNCTION f_autogen_produt_id() RETURNS TRIGGER AS
+$$
+DECLARE
+	next_val INT;
+BEGIN
+	SELECT nextval('id_seq') into next_val;
+	IF NEW.region_id >= next_val THEN
+		PERFORM setval('id_seq', new.region_id+1);
+	ELSIF NEW.region_id = 0 THEN
+		NEW.region_id = next_val;
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+
+CREATE TRIGGER autogen_product_id
+	BEFORE INSERT ON product
+	FOR EACH ROW
+	EXECUTE PROCEDURE f_autogen_product_id();
+
+
